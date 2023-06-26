3cff106dbf06bd4a98854fd0695299f9c6d164db
adiaviad <adaviad4@gmail.com>
2023-06-26 16:47:32 +0300

    renamed test functions

--- test_CI.py
+++ test_CI.py
@@ -1,25 +1,31 @@
 import main
 import pytest
 
-def test_login():
+
+# both password and username adhere to the rules
+def test_login_normal():
     assert main.verify_login("user", "123456")
 
 
-def test_one():
+# username is fine but the password isn't, function should return False
+def test_non_string_password():
     assert not main.verify_login("user", 124321)
 
 
-def test_two():
-    assert not main.verify_login("משתמש", 124321)
+#  password is fine but username is not, function should return False
+def test_non_alphanumeric_useranme():
+    assert not main.verify_login("משתמש", "124321")
 
 
-def test_three():
+# username is fine but password is too short, function should return False
+def test_password_too_short():
     assert not main.verify_login("user", "123")
 
 
-def test_four():
-    assert not main.verify_login("asdo1", "pard")
+def test_normal_add_functionality():
+    assert main.add(1, 2) == 3
+
 
 @pytest.mark.xfail(raises=TypeError)
-def test_type_Error():
+def test_type_error():
     main.add("sd", 2)
